cf00db184c1f5c9756d3a2e063d087c7e8beca33
emiliocorpus <emiliocorpus@gmail.com>
2017-02-13 20:23:19 -0800

    addes example for rating

--- spec/models/dad_joke_spec.rb
+++ spec/models/dad_joke_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe DadJoke, type: :model do
 	let(:dad_joke) { DadJoke.create(joke: "Why do zoo animals make the worst test takers?", answer: "Because they have cheetahs.") }
-	it 'should have a joke' do
-		dad_joke.joke.should_be "Why do animals make the worst test takers?"
+	it 'should have a joke and answer' do
+		expect(dad_joke.joke).to eq "Why do zoo animals make the worst test takers?"
+		expect(dad_joke.answer).to eq "Because they have cheetahs."
 	end
+	it 'should have a rating of 0 for a freshly made joke' do
+		expect(dad_joke.rating).to eq 0
+	end
+	
 end
