2c8abc7b9b41f1551b6a7068dbc6a6e38f0fab5a
emiliocorpus <emiliocorpus@gmail.com>
2017-02-13 20:17:43 -0800

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170214041547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dad_jokes", force: :cascade do |t|
+    t.string   "joke"
+    t.string   "answer"
+    t.float    "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
